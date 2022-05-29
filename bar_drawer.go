bdbaf0818cdfe32f2575d8b2adcfbadc623c19b9
motoki317 <motoki317@gmail.com>
2022-05-29 12:24:40 +0900

    add simple RMS drawer

--- bar_drawer.go
+++ bar_drawer.go
@@ -1,7 +1,11 @@
 package waveform
 
+import (
+	"math"
+)
+
 // BarDrawer determines the bar position and height.
-// samples is a sequence of samples normalized in [-1, 1].
+// samples is a sequence of PCM samples normalized in [-1, 1].
 // BarDrawer should return y, h in range of [0, 1].
 type BarDrawer func(samples []float64) (y, h float64)
 
@@ -25,4 +29,14 @@ var DrawerMinMax BarDrawer = func(samples []float64) (y, h float64) {
 	min = (min + 1) / 2
 	max = (max + 1) / 2
 	return min, max - min
+}
+
+// DrawerRMS is a bar drawer based on RMS (root-mean-square).
+var DrawerRMS BarDrawer = func(samples []float64) (y, h float64) {
+	var squareSum float64
+	for _, s := range samples {
+		squareSum += s * s
+	}
+	rms := math.Sqrt(squareSum / float64(len(samples)))
+	return 0.5 - (rms / 2), rms
 }
